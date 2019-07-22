ccfcf9a862c103bc222108644d78e46d23464e4a
dalianzhouzf <dalian.zhouzf@gmail.com>
2019-07-22 17:07:05 +0900

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+  RouterModule.forRoot([
+      {path: '', component: ProductListComponent },
+      {path: 'products/:id', component: ProductDetailComponent},
+    ]) 
+  ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
